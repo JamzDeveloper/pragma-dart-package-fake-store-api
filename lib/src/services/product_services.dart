a9c699be71b7fc85905fdfdf7b5c8bfa5be342a7
jamz <jamzdeveloper@gmail.com>
2025-07-15 04:08:40 -0500

    newVersion

--- lib/src/services/product_services.dart
+++ lib/src/services/product_services.dart
@@ -1,14 +1,16 @@
 import 'dart:convert';
 import 'package:fake_store_client/src/config/environment.dart';
+import 'package:fake_store_client/src/models/failure.dart';
 import 'package:http/http.dart' as http;
 import 'package:dartz/dartz.dart';
 import '../models/product.dart';
 
 class ProductService {
-
   Future<Either<String, ProductModel>> getProduct(int id) async {
     try {
-      final response = await http.get(Uri.parse('${Environment.baseUrlApiService}/products/$id'));
+      final response = await http.get(
+        Uri.parse('${Environment.baseUrlApiService}/products/$id'),
+      );
 
       if (response.statusCode == 200) {
         final json = jsonDecode(response.body);
@@ -19,6 +21,24 @@ class ProductService {
       }
     } catch (e) {
       return Left('Excepción: $e');
+    }
+  }
+
+  Future<Either<Failure, List<ProductModel>>> fetchProducts() async {
+    try {
+      final url = Uri.parse("${Environment.baseUrlApiService}/products");
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final List list = jsonDecode(response.body);
+        final products = list.map((e) => ProductModel.fromJson(e)).toList();
+
+        return Right(products);
+      } else {
+        return Left(Failure('Error ${response.statusCode}: ${response.body}'));
+      }
+    } catch (err) {
+      return Left(Failure('Excepcion $err'));
     }
   }
 }
